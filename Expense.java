6039dc5bd3cd41dba18113971148a6e189eba680
maringo-anthony <54919602+maringo-anthony@users.noreply.github.com>
2020-03-29 16:41:05 -0400

    Made Expense POJO

---
+++ Expense.java
@@ -0,0 +1,20 @@
+package com.example.hoohacks;
+
+public class Expense {
+
+    private String date;
+    private String name;
+    private double cost;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
