0c82716f3cf30b39c61af2686657ad088bd3c78c
maringo-anthony <54919602+maringo-anthony@users.noreply.github.com>
2020-03-29 17:33:49 -0400

    Src files

---
+++ app/src/main/java/com/example/hoohacks/PastExpenses.java
@@ -0,0 +1,14 @@
+package com.example.hoohacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastExpenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_expenses);
+    }
+}
